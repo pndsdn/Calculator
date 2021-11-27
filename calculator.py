31aeed04ea91c6929d638050722cb057ecedd92e
pndsdn <saa020403@gmail.com>
2021-11-27 23:48:10 +0300

    general operations

--- calculator.py
+++ calculator.py
@@ -7,16 +7,19 @@ class Calculator(QWidget):
 
         self.vbox = QVBoxLayout(self)
         self.hbox_input = QHBoxLayout()
+        self.hbox_zero = QHBoxLayout()
         self.hbox_first = QHBoxLayout()
         self.hbox_second = QHBoxLayout()
+        self.hbox_third = QHBoxLayout()
         self.hbox_result = QHBoxLayout()
-        self.hbox_clr = QHBoxLayout()
 
         self.vbox.addLayout(self.hbox_input)
-        self.vbox.addLayout(self.hbox_first)
+        self.vbox.addLayout(self.hbox_third)
         self.vbox.addLayout(self.hbox_second)
+        self.vbox.addLayout(self.hbox_first)
+        self.vbox.addLayout(self.hbox_zero)
         self.vbox.addLayout(self.hbox_result)
-        self.vbox.addLayout(self.hbox_clr)
+
         self.input = QLineEdit(self)
         self.hbox_input.addWidget(self.input)
 
@@ -29,33 +32,68 @@ class Calculator(QWidget):
         self.b_3 = QPushButton("3", self)
         self.hbox_first.addWidget(self.b_3)
 
+        self.b_4 = QPushButton("4", self)
+        self.hbox_second.addWidget(self.b_4)
+
+        self.b_5 = QPushButton("5", self)
+        self.hbox_second.addWidget(self.b_5)
+
+        self.b_6 = QPushButton("6", self)
+        self.hbox_second.addWidget(self.b_6)
+
+        self.b_7 = QPushButton("7", self)
+        self.hbox_third.addWidget(self.b_7)
+
+        self.b_8 = QPushButton("8", self)
+        self.hbox_third.addWidget(self.b_8)
+
+        self.b_9 = QPushButton("9", self)
+        self.hbox_third.addWidget(self.b_9)
+
+        self.b_bs = QPushButton("bs", self)
+        self.hbox_zero.addWidget(self.b_bs)
+
+        self.b_0 = QPushButton("0", self)
+        self.hbox_zero.addWidget(self.b_0)
+
+        self.b_dot = QPushButton(".", self)
+        self.hbox_zero.addWidget(self.b_dot)
+
         self.b_plus = QPushButton("+", self)
-        self.hbox_second.addWidget(self.b_plus)
+        self.hbox_third.addWidget(self.b_plus)
 
         self.b_minus = QPushButton("-", self)
         self.hbox_second.addWidget(self.b_minus)
 
-        self.b_multiple = QPushButton("*", self)
-        self.hbox_second.addWidget(self.b_multiple)
+        self.b_multiply = QPushButton("*", self)
+        self.hbox_first.addWidget(self.b_multiply)
 
-        self.b_division = QPushButton("/", self)
-        self.hbox_second.addWidget(self.b_division)
+        self.b_divide = QPushButton("/", self)
+        self.hbox_zero.addWidget(self.b_divide)
 
         self.b_result = QPushButton("=", self)
         self.hbox_result.addWidget(self.b_result)
 
-        self.b_result = QPushButton("Clr", self)
-        self.hbox_clr.addWidget(self.b_result)
+        self.b_clr = QPushButton("clr", self)
+        self.hbox_result.addWidget(self.b_clr)
 
         self.b_plus.clicked.connect(lambda: self._operation("+"))
         self.b_minus.clicked.connect(lambda: self._operation("-"))
-        self.b_multiple.clicked.connect(lambda: self._operation("*"))
-        self.b_division.clicked.connect(lambda: self._operation("/"))
+        self.b_multiply.clicked.connect(lambda: self._operation("*"))
+        self.b_divide.clicked.connect(lambda: self._operation("/"))
         self.b_result.clicked.connect(self._result)
 
         self.b_1.clicked.connect(lambda: self._button("1"))
         self.b_2.clicked.connect(lambda: self._button("2"))
         self.b_3.clicked.connect(lambda: self._button("3"))
+        self.b_4.clicked.connect(lambda: self._button("4"))
+        self.b_5.clicked.connect(lambda: self._button("5"))
+        self.b_6.clicked.connect(lambda: self._button("6"))
+        self.b_7.clicked.connect(lambda: self._button("7"))
+        self.b_8.clicked.connect(lambda: self._button("8"))
+        self.b_9.clicked.connect(lambda: self._button("9"))
+        self.b_0.clicked.connect(lambda: self._button("0"))
+        self.b_dot.clicked.connect(lambda: self._button("."))
 
     def _button(self, param):
         line = self.input.text()
@@ -70,12 +108,15 @@ class Calculator(QWidget):
         self.num_2 = int(self.input.text())
         if self.op == "+":
             self.input.setText(str(self.num_1 + self.num_2))
-        elif self.op == "-":
+
+        if self.op == "-":
             self.input.setText(str(self.num_1 - self.num_2))
-        elif self.op == "*":
+
+        if self.op == "*":
             self.input.setText(str(self.num_1 * self.num_2))
-        elif self.op == "/":
-            self.input.setText(str(float(self.num_1) / float(self.num_2)))
+
+        if self.op == "/":
+            self.input.setText(str(self.num_1 / self.num_2))
 
 
 app = QApplication(sys.argv)
